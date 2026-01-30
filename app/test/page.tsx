8da011891a2662a81f652f52e5d53734fe682a12
Mohammed Anas <anasmd021@gmail.com>
2026-01-30 14:41:31 +0530

    repo

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -15,20 +15,6 @@ export default function TestPage() {
         <source src="/30001-0120.webm" type="video/webm" />
         Your browser does not support the video tag.
       </video>
-      <video
-        autoPlay
-        loop
-        muted
-        playsInline
-        preload="auto"
-        className="alphaVideo w-[480px]"
-      >
-        {/* Safari (HEVC + alpha) */}
-        <source src="/8.hevc.mp4" type='video/mp4; codecs="hvc1"' />
-        {/* Chrome / Firefox */}
-        <source src="/8.webm" type="video/webm" />
-        Your browser does not support the video tag.
-      </video>
     </div>
   );
 }
